02184ac29d2fbff3f260649fc533dff24ea4426f
Erik <ol.erik0107@gmail.com>
2024-03-19 06:23:07 -0300

    feat: adapting names of controllers

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,9 +2,9 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
-import { authenticate } from './autheticate-controller'
-import { profile } from './profile-controller'
-import { register } from './register-controller'
+import { authenticate } from './autheticate'
+import { profile } from './profile'
+import { register } from './register'
 
 export const userRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
